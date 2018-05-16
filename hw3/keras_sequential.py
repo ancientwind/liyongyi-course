942932ba6c506325caad0df4b585cf624b60d2ce
wayne zeng <zengqw62@163.com>
2018-05-16 22:24:06 +0800

    train, evaluate, test, and predict in basic full connect nn

--- hw3/keras_sequential.py
+++ hw3/keras_sequential.py
@@ -1,14 +1,37 @@
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.utils import np_utils
 
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+def load_data(number=10000):
+    
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train[0:number]
+    y_train = y_train[0:number]
 
-print(x_train.shape, x_test.shape)
+    # 3 dim -> 2 dim
+    x_train = x_train.reshape(number, 28*28)
+    x_test = x_test.reshape(x_test.shape[0], 28*28) 
+
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+
+    # number -> matrix representation of the number
+    y_train = np_utils.to_categorical(y_train, 10)
+    y_test = np_utils.to_categorical(y_test, 10)
+
+    print(x_train.shape, y_train[0])
+
+    # nomailize to 0~1
+    x_train = x_train / 255
+    x_test = x_test / 255
+
+    print(x_train[0])
+    return (x_train, y_train), (x_test, y_test)
 
 # image = x_train[0]
 # plt.imshow(image, 'gray')
@@ -20,19 +43,37 @@ print(x_train.shape, x_test.shape)
 
 model = Sequential()
 
-model.add(Dense(input_dim=28*28, units=500, activation='relu'))
-model.add(Dense(units=500, activation='relu'))
+model.add(Dense(input_dim=28*28, units=700, activation='relu'))
+model.add(Dense(units=700, activation='relu'))
 model.add(Dense(units=10, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy',
 optimizer='adam',
 metrics=['accuracy'])
 
-xto_train=x_train[:5000,]
-yto_train=y_train[:5000,]
+(x_train, y_train), (x_test, y_test) = load_data(5000)
 
-model.fit(xto_train,yto_train, batch_size=1000, epochs=20)
+xto_train = x_train[0:4500]
+xto_evaluate = x_train[4500:5000]
+yto_train = y_train[0:4500]
+yto_evaluate = y_train[4500:5000]
 
-score=model.evaluate(x_test[:500], y_test[:500])
+model.fit(xto_train, yto_train, batch_size=100, epochs=20)
+
+score=model.evaluate(xto_evaluate, yto_evaluate)
 print('Total loss:', score[0])
 print('Accuracy:', score[1])
+
+result=model.evaluate(x_test[:500], y_test[:500])
+print(result)
+
+classes = model.predict(x_test[:100])
+print(classes[:3])
+print('origin: ', y_test[0])
+print(np.argmax(classes[0]))
+
+print('origin: ', y_test[1])
+print(np.argmax(classes[1]))
+
+print('origin: ', y_test[97])
+print(np.argmax(classes[97]))
